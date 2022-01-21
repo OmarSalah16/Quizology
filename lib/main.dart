a9812082204dc172fdc54006661c8ee7942a85f5
Mohamed <amin44061@gmail.com>
2022-01-21 18:28:17 +0200

    push notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,36 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'package:opentrivia_quiz_game_final/screens/home.dart';
+import 'package:opentrivia_quiz_game_final/screens/sign_in.dart';
 import 'screens/splash_screen.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:opentrivia_quiz_game_final/services/local_notification_service.dart';
 
-void main() async
-{
+Future<void> backgroundHandler(RemoteMessage message) async {
+  print(message.data.toString());
+  print(message.notification!.title);
+}
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  LocalNotificationService.initialize;
+  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
   await Firebase.initializeApp();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget
-{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context)
-  {
-    return MaterialApp
-    (
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: 'Quizology',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primaryColor: const Color(0xff393d4e)),
-      home: SplashScreen()
+      home: SplashScreen(),
+      routes: {
+        "login": (_) => SplashScreen(),
+        "home": (_) => Home(),
+      },
     );
   }
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,46 +1,60 @@
-
 import 'dart:async';
-
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:opentrivia_quiz_game_final/screens/intro.dart';
+import 'package:opentrivia_quiz_game_final/services/local_notification_service.dart';
 
-class SplashScreen extends StatefulWidget
-{
+class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen>
-{
+class _SplashScreenState extends State<SplashScreen> {
   @override
-  void initState()
-  {
+  void initState() {
     super.initState();
-    Timer(const Duration(seconds: 3), ()
-    {
-      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => Intro()));
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (_) => Intro()));
+    });
+    FirebaseMessaging.instance.getInitialMessage().then((message) {
+      if (message != null) {
+        final routeFromMessage = message.data["route"];
+        Navigator.of(context).pushNamed(routeFromMessage);
+      }
+    });
+
+    ///forground work only
+    FirebaseMessaging.onMessage.listen((message) {
+      if (message.notification != null) {
+        print(message.notification!.body);
+        print(message.notification!.title);
+      }
+      LocalNotificationService.display(message);
+    });
+
+    ///When the app is in background but opened and user taps on the notification
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {
+      final routeFromMessage = message.data["route"];
+      Navigator.of(context).pushNamed(routeFromMessage);
     });
   }
 
   @override
-  Widget build(BuildContext context)
-  {
-    return Scaffold
-    (
-      backgroundColor: const Color(0xff393d4e),
-      body: Center
-      (
-        child: Column
-        (
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: 
-          [
-            Image.asset('assets/splash.png', height: 150),
-            Text("Quizology", style: TextStyle(color: Color(0xffff7126), fontSize: 20, fontWeight: FontWeight.w700))
-          ]
-        )
-      )
-    );
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color(0xff393d4e),
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+              Image.asset('assets/splash.png', height: 150),
+              Text("Quizology",
+                  style: TextStyle(
+                      color: Color(0xffff7126),
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700))
+            ])));
   }
 }
